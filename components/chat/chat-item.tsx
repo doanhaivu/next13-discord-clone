ca2fc0137cd63bcdd825b407ccc22e50ccd7eb3b
Vu Doan <vu@doanvuh.com>
2023-10-01 08:55:57 -0700

    Build error

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -131,7 +131,7 @@ export const ChatItem = ({
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
         <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
-          <UserAvatar src={member.profile.imageUrl} />
+          <UserAvatar src={member.profile.imageUrl!} />
         </div>
         <div className="flex flex-col w-full">
           <div className="flex items-center gap-x-2">
